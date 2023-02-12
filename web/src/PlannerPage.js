308557440ec7642ae3ff1e316475a3b819be1c58
0x9576 <gahyu22@gmail.com>
2023-02-12 20:56:20 +0900

    css
    css변경

--- web/src/PlannerPage.js
+++ web/src/PlannerPage.js
@@ -3,19 +3,14 @@ export default function PlannerPage({ $target, initialState }) {
     this.state = initialState
 
     const $page = document.createElement('div')
-    $page.className = 'PlannerPage'
+    const $todo = document.createElement('div')
+    const $calendar = document.createElement('div')
 
-    $page.innerHTML = `
-    <div class='day_box'>
-        <div class='day sun'>SUN</div>
-        <div class='day'>MON</div>
-        <div class='day'>TUE</div>
-        <div class='day'>WED</div>
-        <div class='day'>THU</div>
-        <div class='day'>FRI</div>
-        <div class='day sat'>SAT</div>
-    </div>
-    `
+    $calendar.className = 'Calendar'
+    $page.className = 'PlannerPage'
+    $todo.className = 'Todo'
+    $todo.id = 'Todo'
+    $todo.innerHTML = 'todo'
 
     this.setState = nextState => {
         this.state = nextState
@@ -23,7 +18,19 @@ export default function PlannerPage({ $target, initialState }) {
     }
 
     this.render = () => {
-        var arr = '';
+
+        $calendar.innerHTML = `
+        <div class='day_box'>
+            <div class='day sun'>SUN</div>
+            <div class='day'>MON</div>
+            <div class='day'>TUE</div>
+            <div class='day'>WED</div>
+            <div class='day'>THU</div>
+            <div class='day'>FRI</div>
+            <div class='day sat'>SAT</div>
+        </div>
+        `
+
         const date = this.state.date
         const toYear = date.getFullYear();
         const toMonth = date.getMonth();
@@ -41,29 +48,54 @@ export default function PlannerPage({ $target, initialState }) {
 
         // 지난달
         for (let i = prevDate - prevDay; i <= prevDate; i++) {
-            $page.innerHTML += `<div class='date not'>${i}</div>`
+            $calendar.innerHTML += `<div class='date not'>${i}</div>`
         }
 
         // 현재 달
         for (let i = 1; i <= lastDate; i++) {
             if ((firstDay + i) % 7 === 0) {
-                $page.innerHTML += `<div class='date sat'>${i}</div>`
+                $calendar.innerHTML += `<div class='date sat'>${i}</div>`
             }
             else if ((firstDay + i) % 7 === 1) {
-                $page.innerHTML += `<div class='date sun'>${i}</div>`
+                $calendar.innerHTML += `<div class='date sun'>${i}</div>`
             }
             else {
-                $page.innerHTML += `<div class='date'>${i}</div>`
+                $calendar.innerHTML += `<div class='date'>${i}</div>`
             }
         }
 
         // 다음 달
         for (let i = 1; i <= 6 - lastDay; i++) {
-            console.log(lastDay);
-            $page.innerHTML += `<div class='date not'>${i}</div>`
+            $calendar.innerHTML += `<div class='date not'>${i}</div>`
         }
+        $page.appendChild($calendar)
+        $page.appendChild($todo)
 
-        $target.innerHTML = `<h1>${month}</h1>`
+        $target.innerHTML = `
+        <div class = 'PlannerHeader'>
+        <div id = 'prev'><</div>
+        <h1>${month}</h1>
+        <div id = 'next'>></div>
+        </div>
+        `
         $target.appendChild($page)
     }
+
+    this.render()
+
+    document.getElementById('next').addEventListener('click', (e) => {
+        console.log("qqq");
+        const date = this.state.date
+        const toYear = date.getFullYear();
+        const toMonth = date.getMonth();
+        this.setState(new Date(toYear, toMonth + 1, 1))
+    })
+
+    document.getElementById('prev').addEventListener('click', (e) => {
+        const date = this.state.date
+        console.log(date);
+        const toYear = date.getFullYear();
+        const toMonth = date.getMonth();
+        this.setState(new Date(toYear, toMonth - 1, 1))
+    })
 }
